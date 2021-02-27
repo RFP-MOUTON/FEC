69f0a139779b000c6c53ff50db100fb1cfbec722
Chris McVey <chris.mcvey@gmail.com>
2021-02-26 16:11:44 -0800

    HouseKeeping

--- tests/setup.js
+++ tests/setup.js
@@ -3,4 +3,4 @@ import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({ adapter: new Adapter() });
 // This tells enzyme that we're going to be using it each time it was
-console.log("something")
+console.log('something');
