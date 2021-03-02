b6101fcf9197701726afc7623524bc7209c30c57
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2021-03-02 15:30:07 -0800

    Sucessfully computing an average for all reviews on a specific product.

--- src/Components/Overview/Ratings.jsx
+++ src/Components/Overview/Ratings.jsx
@@ -3,36 +3,7 @@ import axios from 'axios';
 import instance from './instance';
 import StarIcon from './StarIcon.jsx';
 
-// class Ratings extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       ratings: this.props.ratings,
-//       average: '',
-//       count: '',
-//     };
-
-//   }
-//   render() {
-//     return (
-//       <div>
-//         Average Reviews:
-//         <div>{this.state.average}</div>
-//       </div>
-//     );
-//   }
-// }
 function Ratings(props) {
-  // const {
-  //   index,
-  //   rating,
-  //   hoverRating,
-  //   onMouseEnter,
-  //   onMouseLeave,
-  //   onSaveRating,
-  // } = props;
-  // const fill = React.useMemo(() =>)
   let count = 0;
   let average = 0;
   if (props.ratings !== undefined) {
