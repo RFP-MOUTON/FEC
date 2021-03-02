19b8b78c0a1d53cb0dd938b191a3d3b941201b12
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2021-03-02 15:41:49 -0800

    Small linting change

--- src/Components/Overview/instance.js
+++ src/Components/Overview/instance.js
@@ -4,6 +4,6 @@ const instance = axios.create({
   baseURL: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/'
 });
 
-instance.defaults.headers.common['Authorization'] = '0e1e30a63688b9573520a3ec8d5ed5be0248561e';
+instance.defaults.headers.common.Authorization = '0e1e30a63688b9573520a3ec8d5ed5be0248561e';
 
 export default instance;
