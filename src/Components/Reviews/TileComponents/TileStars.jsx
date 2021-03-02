e1d5d67159561dfe20843fa92c3ea66274d4376b
Chris McVey <chris.mcvey@gmail.com>
2021-03-01 22:36:39 -0800

    Stars dynamically rendering!!!

--- src/Components/Reviews/TileComponents/TileStars.jsx
+++ src/Components/Reviews/TileComponents/TileStars.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
 
+import Star from './Star.jsx';
+
 const TileStars = ({ rating }) => {
+  let currentRating = rating;
+  let starArray = [];
+
+  for (let i = 0; i < 5; i++) {
+    if (currentRating === 0) {
+      starArray.push({shade: "0%", id: Math.random()});
+    } else if (currentRating >= 1) {
+      starArray.push({shade: "100%", id: Math.random()});
+      currentRating -= 1;
+    }
+  }
   return (
     <div>
       <div>{rating}</div>
-      <svg
-        className="w-6 h-6"
-        fill="url(#grad)"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <linearGradient id="grad">
-          <stop offset="50%" stopColor="black" />
-          <stop offset="50%" stopColor="white" />
-        </linearGradient>
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-        />
-      </svg>
+      {starArray.map((shade) => {
+        return <Star key={shade.id} id={shade.id} shade={shade.shade} />;
+      })}
     </div>
   );
 };
