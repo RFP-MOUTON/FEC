cdcb0be705ef03af8b112a8bea7b7dd1566d49de
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2021-03-03 10:47:56 -0800

    Hopefully this will fix the git issues for now, temporary fix.

--- src/Components/Overview/instance.js
+++ src/Components/Overview/instance.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/'
+  baseURL: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/',
 });
 
 instance.defaults.headers.common.Authorization =
-  '0e1e30a63688b9573520a3ec8d5ed5be0248561e';
+  '5fa4a045e262d8dbf093e21851f9bbf70e30e552';
 
 export default instance;
