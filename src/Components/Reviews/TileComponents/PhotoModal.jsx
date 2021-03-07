6f5acf1278051b39780fbdae60cab2768d2f0641
Chris McVey <chris.mcvey@gmail.com>
2021-03-07 08:44:59 -0800

    Styling

--- src/Components/Reviews/TileComponents/PhotoModal.jsx
+++ src/Components/Reviews/TileComponents/PhotoModal.jsx
@@ -3,15 +3,17 @@ import React from 'react';
 const PhotoModal = ({ photo, toggle }) => {
   return (
     <div className="blur">
-      <img
-        className="modal-main"
-        key={photo.id}
-        src={photo.url}
-        alt={photo.url}
-      />
-      <button type="button" className="closeButton" onClick={toggle}>
-        X
-      </button>
+      <div className="photoContainer">
+        <img
+          className="modal-main"
+          key={photo.id}
+          src={photo.url}
+          alt={photo.url}
+        />
+        <button type="button" className="closeButton" onClick={toggle}>
+          X
+        </button>
+      </div>
     </div>
   );
 };
