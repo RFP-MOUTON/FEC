6d32be1401ef0d9d916427089fef72b7377a241b
Chris McVey <chris.mcvey@gmail.com>
2021-03-09 15:34:18 -0800

    handling edge cases

--- src/Components/Reviews/ReviewList.jsx
+++ src/Components/Reviews/ReviewList.jsx
@@ -3,18 +3,22 @@ import React from 'react';
 import ReviewTile from './ReviewTile.jsx';
 
 const ReviewList = ({ reviews, handleHelpful, report }) => {
+  console.log(reviews)
   return (
     <>
       <div id="reviewList">
         {reviews.map((review) => {
-          return (
-            <ReviewTile
-              key={review.review_id}
-              review={review}
-              handleHelpful={handleHelpful}
-              report={report}
-            />
-          );
+          if (review) {
+            return (
+              <ReviewTile
+                key={review.review_id}
+                review={review}
+                handleHelpful={handleHelpful}
+                report={report}
+              />
+            );
+          }
+
         })}
       </div>
     </>
