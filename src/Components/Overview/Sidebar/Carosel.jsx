f3035b44c0a5c0049687c41411929c1df8b16582
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2021-03-09 14:24:56 -0800

    Completed basic carousel main image functionality and rendering thumbnail list

--- src/Components/Overview/Sidebar/Carosel.jsx
+++ src/Components/Overview/Sidebar/Carosel.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CarouselThumbnail from './CarouselThumbnail.jsx';
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
 
 class Carosel extends React.Component {
@@ -17,19 +18,39 @@ class Carosel extends React.Component {
     });
   }
 
+  componentDidUpdate(prevProps) {
+    const { styleID } = this.props;
+    const { styleHistory } = this.state;
+    if (styleID !== prevProps.styleID && styleHistory !== undefined) {
+      if (!styleHistory[styleID]) {
+        this.setState({
+          selectedImage: 0,
+        });
+      } else {
+        this.setState({
+          selectedImage: styleHistory[styleID],
+        });
+      }
+    }
+    // const styleString = styleID.toString();
+  }
+
   rightArrowClick() {
-    let { selectedImage } = this.state;
+    const { selectedImage } = this.state;
     const { images, styleID } = this.props;
     if (images.length - 1 <= selectedImage) {
-      console.log('click');
       this.setState({
         selectedImage: 0,
-        [styleID]: 0,
+        styleHistory: {
+          [styleID]: 0,
+        },
       });
     } else {
       this.setState({
         selectedImage: selectedImage + 1,
-        [styleID]: selectedImage + 1,
+        styleHistory: {
+          [styleID]: selectedImage + 1,
+        },
       });
     }
   }
@@ -40,12 +61,16 @@ class Carosel extends React.Component {
     if (selectedImage >= 0) {
       this.setState({
         selectedImage: images.length - 1,
-        [styleID]: images.length - 1,
+        styleHistory: {
+          [styleID]: 0,
+        },
       });
     } else {
       this.setState({
         selectedImage: selectedImage - 1,
-        [styleID]: images.length - 1,
+        styleHistory: {
+          [styleID]: selectedImage - 1,
+        },
       });
     }
   }
@@ -59,12 +84,13 @@ class Carosel extends React.Component {
   render() {
     const { images, styleID } = this.props;
     const { selectedImage } = this.state;
+    let thumbnailIndex = 0;
     if (images === '' || images === undefined) {
       return <div />;
     }
 
     return (
-      <div className="slider">
+      <div className="imageCarousel">
         <FaArrowAltCircleLeft
           className="left-arrow"
           onClick={this.leftArrowClick}
@@ -76,8 +102,20 @@ class Carosel extends React.Component {
         <img
           src={images[selectedImage].url}
           alt="MainImage"
-          className="caroselImage"
+          className="carouselImage"
         />
+        <div className="carouselThumbnails">
+          {images.map((image) => {
+            thumbnailIndex += 1;
+            return (
+              <CarouselThumbnail
+                thumbNail={image.thumbnail_url}
+                index={thumbnailIndex - 1}
+                currentImageIndex={selectedImage}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
---
+++ src/Components/Overview/Sidebar/CarouselThumbnail.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class CarouselThumbnail extends React.component {
+  constructor(props) {
+    super(props);
+    this.state({
+      displayedPage: 1,
+    });
+  }
+
+  // handleUpArrow() {
+
+  // }
+
+  // handleDownArrow() {
+  //   if()
+  // }
+
+  render() {
+    const { thumbNail, index, currentImageIndex } = this.props;
+    if (index === currentImageIndex) {
+      return (
+        <>
+          <img
+            src={thumbNail}
+            alt={index}
+            className="selectedCarouselThumbnail"
+          />
+        </>
+      );
+    }
+    return <img src={thumbNail} alt={index} className="carouselThumbnail" />;
+  }
+}
+
+export default CarouselThumbnail;
